fafd0d7500cb03c3267a3cb175a759dfa3c046a8
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2022-11-03 15:09:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ from tkinter import messagebox
 import os
 
 
-# add widgets here
+# Definer vinduet med tkinter
 window=Tk()
 window.title('Showinfo')
 window.geometry("500x300+20+20")
 
-def close():
+# Luk funktion
+def close():            
     window.destroy()
 
-def about():
+# besked funktion om programmet.
+def about():            
         svar = messagebox.askokcancel('About', 'Contact frank@simens.dk \nProjektet kan findes på https://github.com/Hvemmse/ \nLicens GPL 3 \nhttps://www.112support.dk"')
 
         if svar:
             messagebox.showinfo('Tak', 'Tak Fordi du bruger programmet')
-            
 
-
+# Kopier Funktion koperet inholdet af tekstfilen status.txt til Udklipsholder.          
 def kopier():
     os.system("cat ./status.txt | xsel -i -b")
-    
+
+# Viser info fra update.sh filen til tekstbox, og som fil status.txt
 def visinfo():
     os.system("sh ./status > status.txt")
     
@@ -35,28 +37,34 @@ def visinfo():
     data = f.read()
     txtarea.insert (END, data)
 
+# Overskriften i Programmet
 lbl=Label(window, text="Her vises nogle info om systemet.", fg='black', font=("Helvetica", 14))
 lbl.place(x=20, y=40)
 
-#definer knapper for neden i Programmet
-
+# definer knapper for neden i Programmet
+# tekst felt
 txtarea = Text(window, width=60, height=30)
 txtarea.pack(pady=60)
 
+# Luk knap
 btnLuk = Button(window, text='luk', command=close)
 btnLuk.bind('<Button-1>')
 btnLuk.place(x=40, y=260)
 
+# About Knap
 btnOK = Button(window, text='About', command=about)
 btnOK.bind('<Button-1>')
 btnOK.place(x=90, y=260)
 
+# Kopier knap
 btnUdskriv = Button(window, text='Kopier', command=kopier)
 btnUdskriv.bind('<Button-1>')
 btnUdskriv.place(x=155, y=260)
 
+# VisInfo knap
 btnVisInfo = Button(window, text='VisInfo', command=visinfo)
 btnVisInfo.bind('<Button-1>')
 btnVisInfo.place(x=225, y=260)
 
+# vis vinduet
 window.mainloop()
